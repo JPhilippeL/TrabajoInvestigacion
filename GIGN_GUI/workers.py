7c1f3a297e21c144942074a4885e3ffd3cb2176a
DarwinMo <mohamed.abdessalame@outlook.fr>
2026-05-08 12:32:23 +0200

    feat : thread for hp tuning

--- GIGN_GUI/workers.py
+++ GIGN_GUI/workers.py
@@ -2,6 +2,7 @@ import traceback
 
 from PySide6.QtCore import QThread, Signal
 
+from GIGN_GUI.controller.hyperparameter_controller import launch_ray_tune_hyperparameter_search
 from GIGN_GUI.model.graph_generation import generate_all_graphs
 from GIGN_GUI.model.predict_gign import predict
 from GIGN_GUI.model.train_gign import train_gign
@@ -84,6 +85,25 @@ class PredictThread(QThread):
         try:
             thread_logger = _ThreadLoggerProxy(self.log_message.emit)
             predict(**self.params, log_callback=thread_logger)
+            self.finished_success.emit()
+        except Exception as _:
+            traceback_error = str(traceback.format_exc())
+            self.finished_error.emit(traceback_error)
+
+
+class HPGIGNThread(QThread):
+    log_message = Signal(str)
+    finished_success = Signal()
+    finished_error = Signal(str)
+
+    def __init__(self, params, parent=None):
+        super().__init__(parent)
+        self.params = params
+
+    def run(self):
+        try:
+            thread_logger = _ThreadLoggerProxy(self.log_message.emit)
+            launch_ray_tune_hyperparameter_search(self.params, log_callback=thread_logger)
             self.finished_success.emit()
         except Exception as _:
             traceback_error = str(traceback.format_exc())
