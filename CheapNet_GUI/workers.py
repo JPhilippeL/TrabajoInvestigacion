1162e046c3677ae0cc0ee65730c918f13f330525
DarwinMo <mohamed.abdessalame@outlook.fr>
2026-05-11 12:53:40 +0200

    feat(CheapNet) : implements a process to run externally the hyperparameter tuning

--- CheapNet_GUI/workers.py
+++ CheapNet_GUI/workers.py
@@ -1,7 +1,9 @@
+import json
 import logging
+import sys
 import traceback
 
-from PySide6.QtCore import QThread, Signal
+from PySide6.QtCore import QThread, Signal, QObject, QProcess
 
 from CheapNet_GUI.model.graph_generation import generate_all_graphs
 from CheapNet_GUI.model.predict import predict
@@ -85,3 +87,49 @@ class PredictThread(QThread):
         except Exception as _:
             error_detailed = str(traceback.format_exc())
             self.finished_error.emit(error_detailed)
+
+
+class HyperparameterTuningProcess(QObject):
+    log_message = Signal(str)
+    finished_success = Signal()
+    finished_error = Signal(str)
+
+    def __init__(self, params, parent=None):
+        super().__init__(parent)
+        self.params = params
+        self.process = QProcess()
+
+        self.process.readyReadStandardOutput.connect(self.__read_stdout)
+        self.process.readyReadStandardError.connect(self.__read_stderr)
+
+        self.process.finished.connect(self._on_finished)
+        self.process.errorOccurred.connect(self._on_error)
+        self.process.stateChanged.connect(self._on_state_changed)
+
+    def start(self, payload):
+        params = json.dumps(payload)
+        self.process.start(sys.executable, ["-u", "CheapNet_GUI/adapter/hyperparameter_tuning_adapter", params])
+
+    def __read_stdout(self):
+        data = self.process.readAllStandardOutput()
+        stdout = bytes(data).decode("utf-8")
+        self.log_message.emit(stdout)
+
+    def __read_stderr(self):
+        data = self.process.readAllStandardError()
+        stderr = bytes(data).decode("utf-8")
+        self.log_message.emit(stderr)
+
+    def _on_finished(self, exit_code, exit_status):
+        if exit_code == 0:
+            self.finished_success.emit()
+        else:
+            self.finished_error.emit(
+                f"Hyperparameter tuning failed with exit code {exit_code} and code status {exit_status}.")
+
+    def _on_state_changed(self, state):
+        self.log_message.emit(f"Hyperparameter tuning state changed to {state}")
+
+    def _on_error(self, error):
+        error_message = f"Hyperparameter tuning failed with exit code {error}"
+        self.finished_error.emit(f"[HPTUNING]{error_message}")
