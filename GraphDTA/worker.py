c0fc6d10047a5ae12123a79e951cab441100b481
DarwinMo <mohamed.abdessalame@outlook.fr>
2026-05-19 11:45:46 +0200

    feat(Thread GraphDTA): thread to manage graph generation

---
+++ GraphDTA/worker.py
@@ -0,0 +1,47 @@
+import traceback
+
+from PySide6.QtCore import QThread, Signal
+
+from GraphDTA.model.generate_graph import generate_all_dta_dta
+
+
+class _ThreadLoggerProxy:
+    def __init__(self, emit_func):
+        self._emit = emit_func
+
+    def info(self, *args):
+        if not args:
+            return
+        message = " ".join(str(arg) for arg in args)
+        self._emit(message)
+
+    def error(self, *args):
+        if not args:
+            return
+        message = " ".join(str(arg) for arg in args)
+        self._emit(f"ERROR: {message}")
+
+    def debug(self, *args):
+        if not args:
+            return
+        message = " ".join(str(arg) for arg in args)
+        self._emit(f"DEBUG: {message}")
+
+
+class DBGenerationThread(QThread):
+    finished_success = Signal()
+    finished_error = Signal(str)
+    log_message = Signal(str)
+
+    def __init__(self, params, parent=None):
+        super().__init__(parent)
+        self.params = params
+
+    def run(self):
+        try:
+            thread_logger = _ThreadLoggerProxy(self.log_message.emit)
+            generate_all_dta_dta(**self.params)
+            self.finished_success.emit()
+        except Exception as _:
+            traceback_error = str(traceback.format_exc())
+            self.finished_error.emit(traceback_error)
