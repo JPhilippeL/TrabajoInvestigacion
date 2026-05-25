13257915f607704e8c1bd12669a7927b28cd2eb8
DarwinMo <mohamed.abdessalame@outlook.fr>
2026-05-25 11:30:51 +0200

    feat(GraphDTA) : add a thread for prediction action

--- GraphDTA/worker.py
+++ GraphDTA/worker.py
@@ -3,6 +3,8 @@ import traceback
 from PySide6.QtCore import QThread, Signal
 
 from GraphDTA.model.generate_graph import generate_all_dta_dta
+from GraphDTA.model.predict import predict
+from GraphDTA.model.train import train
 
 
 class _ThreadLoggerProxy:
@@ -41,6 +43,44 @@ class DBGenerationThread(QThread):
         try:
             thread_logger = _ThreadLoggerProxy(self.log_message.emit)
             generate_all_dta_dta(**self.params)
+            self.finished_success.emit()
+        except Exception as _:
+            traceback_error = str(traceback.format_exc())
+            self.finished_error.emit(traceback_error)
+
+
+class TrainDTAThread(QThread):
+    finished_success = Signal()
+    finished_error = Signal(str)
+    log_message = Signal(str)
+
+    def __init__(self, params, parent=None):
+        super().__init__(parent)
+        self.params = params
+
+    def run(self):
+        try:
+            thread_logger = _ThreadLoggerProxy(self.log_message.emit)
+            train(**self.params, log_callback=thread_logger)
+            self.finished_success.emit()
+        except Exception as _:
+            traceback_error = str(traceback.format_exc())
+            self.finished_error.emit(traceback_error)
+
+
+class PredictDTAThread(QThread):
+    finished_success = Signal()
+    finished_error = Signal(str)
+    log_message = Signal(str)
+
+    def __init__(self, params, parent=None):
+        super().__init__(parent)
+        self.params = params
+
+    def run(self):
+        try:
+            thread_logger = _ThreadLoggerProxy(self.log_message.emit)
+            predict(**self.params, log_callback=thread_logger)
             self.finished_success.emit()
         except Exception as _:
             traceback_error = str(traceback.format_exc())
