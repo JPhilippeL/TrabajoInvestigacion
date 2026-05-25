530fca433a5ddaa5c2ad0b1b63d1920c6d6a58e7
DarwinMo <mohamed.abdessalame@outlook.fr>
2026-05-25 11:30:28 +0200

    feat(GraphDTA) : add a scaling function

--- GraphDTA/model/utils.py
+++ GraphDTA/model/utils.py
@@ -77,3 +77,19 @@ def seed_everything(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+
+def escala_global(file_path):
+    labels = np.loadtxt(file_path, usecols=1)
+
+    real_min = labels.min()
+    real_max = labels.max()
+
+    margin = 0.2
+    global_min = real_min - margin
+    global_max = real_max + margin
+
+    print("Global axis limits:", global_min, global_max)
+
+    np.save("global_axis.npy", [global_min, global_max])
+
+    return global_min, global_max
