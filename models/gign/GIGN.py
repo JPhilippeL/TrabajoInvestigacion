e94beeb63ffce5fd94b225ae8352a7ee6a078a68
DarwinMo <mohamed.abdessalame@outlook.fr>
2026-06-17 11:13:03 +0200

    feat : train,predict and model services of gign

--- models/gign/GIGN.py
+++ models/gign/GIGN.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 from torch_geometric.nn import global_mean_pool
 
-from GIGN_GUI.model.cheapnet_v02 import MLP, GIGNBlock
+from models.gign.cheapnet_v2 import MLP, GIGNBlock
 
 
 class GIGN(nn.Module):
