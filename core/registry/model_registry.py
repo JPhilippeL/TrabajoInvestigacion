eb9460773649012ef4195edb940c4221bf765ade
DarwinMo <mohamed.abdessalame@outlook.fr>
2026-06-17 11:11:08 +0200

    feat : adding gign to the registry

--- core/registry/model_registry.py
+++ core/registry/model_registry.py
@@ -1,9 +1,18 @@
 from facade_pattern.cheapnet_facade import CheapNetFacade
-
+from facade_pattern.gign_facade import GIGNFacade
 MODEL_REGISTRY = {
     "cheapnet": {
         "display_name": "CheapNet",
         "facade": CheapNetFacade(),
+        "tasks": {
+            "generate_data": "Generate dataset",
+            "train": "Train model",
+            "predict": "Predict",
+        },
+    },
+    "gign": {
+        "display_name": "GIGN",
+        "facade": GIGNFacade(),
         "tasks": {
             "generate_data": "Generate dataset",
             "train": "Train model",
