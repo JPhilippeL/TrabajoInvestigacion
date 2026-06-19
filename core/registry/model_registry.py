909746a6626ddeeda199b8ef5e1125d14155f449
DarwinMo <mohamed.abdessalame@outlook.fr>
2026-06-19 11:20:19 +0200

    feat : add graph dta to the registry

--- core/registry/model_registry.py
+++ core/registry/model_registry.py
@@ -1,5 +1,12 @@
+from graphdta.architecture.GAT import GATNet
+from graphdta.architecture.GAT_GCN import GAT_GCN
+from graphdta.architecture.GCN import GCNNet
+from graphdta.architecture.GINConv import GINConvNet
+
 from facade_pattern.cheapnet_facade import CheapNetFacade
 from facade_pattern.gign_facade import GIGNFacade
+from facade_pattern.graph_dta_facade import DTAFacade
+
 MODEL_REGISTRY = {
     "cheapnet": {
         "display_name": "CheapNet",
@@ -13,6 +20,15 @@ MODEL_REGISTRY = {
     "gign": {
         "display_name": "GIGN",
         "facade": GIGNFacade(),
+        "tasks": {
+            "generate_data": "Generate dataset",
+            "train": "Train model",
+            "predict": "Predict",
+        },
+    },
+    "graphdta": {
+        "display_name": "GraphDTA",
+        "facade": DTAFacade(),
         "tasks": {
             "generate_data": "Generate dataset",
             "train": "Train model",
