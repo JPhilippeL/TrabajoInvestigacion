3418c005bb9aaa68b3c8b45d2598da171ddc5480
DarwinMo <mohamed.abdessalame@outlook.fr>
2026-06-22 15:01:54 +0200

    feat: add planet to the registry

--- core/registry/model_registry.py
+++ core/registry/model_registry.py
@@ -1,6 +1,7 @@
 from facade_pattern.cheapnet_facade import CheapNetFacade
 from facade_pattern.gign_facade import GIGNFacade
 from facade_pattern.graph_dta_facade import DTAFacade
+from facade_pattern.planet_facade import PlanetFacade
 
 MODEL_REGISTRY = {
     "cheapnet": {
@@ -24,6 +25,15 @@ MODEL_REGISTRY = {
     "graph_dta": {
         "display_name": "GraphDTA",
         "facade": DTAFacade(),
+        "tasks": {
+            "generate_data": "Generate dataset",
+            "train": "Train model",
+            "predict": "Predict",
+        },
+    },
+    "planet": {
+        "display_name": "Planet",
+        "facade": PlanetFacade(),
         "tasks": {
             "generate_data": "Generate dataset",
             "train": "Train model",
